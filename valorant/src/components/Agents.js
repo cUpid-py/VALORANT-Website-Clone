277ad17b2fe593b94b554decc542ce8d28673e00
CHR-onicles <tpandivine48@gmail.com>
2021-06-24 18:59:00 +0000

    styled Agents button

--- valorant/src/components/Agents.js
+++ valorant/src/components/Agents.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import "./Agents.css"
-import Raze from "../assets/raze-reyna.png"
+import React from "react";
+import "./Agents.css";
+import Raze from "../assets/raze-reyna.png";
 
 const Agents = () => {
-    return (
-        <div>
-            <div className="agents-sec" >
-                <div className="agents-img" >
-                    <img src={Raze} alt=""   />
-                </div>
-
-                <div className="agents-text" >
-
-                    <h1  className="head" >YOUR <br /> AGENTS</h1>
-
-                    <h3>CREATIVITY IS YOUR GREATEST WEAPON.</h3>
-
-                    <p className="head-p" >More than guns and bullets, you’ll choose an Agent <br /> armed with adaptive,
-                    swift, and lethal abilities that <br /> create opportunities to let your gunplay shine.
-                    No <br /> two Agents play alike, just as no two highlight reels <br /> will look the same.</p>
-
-                    <div className="cta-2" >
-                        <a href=""><button>VIEW ALL AGENTS</button></a> 
-                    </div>
-
-
-                </div>
-
-            </div>
-            
-            
+  return (
+    <div>
+      <div className="agents-sec">
+        <div className="agents-img">
+          <img src={Raze} alt="" />
         </div>
-    )
-}
 
-export default Agents
+        <div className="agents-text">
+          <h1 className="head">
+            YOUR <br /> AGENTS
+          </h1>
+
+          <h3>CREATIVITY IS YOUR GREATEST WEAPON.</h3>
+
+          <p className="head-p">
+            More than guns and bullets, you’ll choose an Agent <br /> armed with
+            adaptive, swift, and lethal abilities that <br /> create
+            opportunities to let your gunplay shine. No <br /> two Agents play
+            alike, just as no two highlight reels <br /> will look the same.
+          </p>
+
+          <div className="cta-2">
+            <button>
+              <a href="#">VIEW ALL AGENTS</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Agents;
