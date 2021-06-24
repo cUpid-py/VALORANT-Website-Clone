90cf0c9b4aef0f33c54adb439e87b9efc37391a1
cUpid-py <59352495+cUpid-py@users.noreply.github.com>
2021-06-24 12:09:04 +0000

    Completed 6/7 sections of the project

--- valorant/src/App.js
+++ valorant/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 import News from "./components/News"
 import Trailer from "./components/Trailer"
 import Agents from "./components/Agents"
+import Maps from "./components/Maps"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <News />
         <Trailer />
         <Agents />
+        <Maps />
         
       </body>
 
@@ -29,3 +31,13 @@ function App() {
 }
 
 export default App;
+
+// Questions
+
+// 1. Why is it that the video no longer covers the full screen?
+        // Tried adjusting the padding of the navbar bg and seemed to work out
+
+// 2. Why is it that there are violet dashes in between the navbar items?
+// 3. Why is it that when i hover over the nav-items the rise up and down?
+// 4. Why is it that the font family for the 'Latest news' does not respond even though i have installed the font?
+// 5. Why is it there is a white line/dash in-between the news section and the trailer section?
